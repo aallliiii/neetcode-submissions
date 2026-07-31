e18708c822e52df62d310037574e6f676715c4c6
Muhammad Ali <144407855+aallliiii@users.noreply.github.com>
2026-08-01 01:39:32 +0500

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        already_seen = {}
+        for i in range (len(nums)):
+            temp = target - nums[i]
+            if temp in already_seen:
+                return [already_seen[temp], i]
+            already_seen[nums[i]] = i
+            
+    
+       
